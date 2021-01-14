8d78fc5406195d4d48e56b840b42933b1d299a02
Yuri <you@teste.com>
2021-01-14 10:13:56 -0300

    Update Cards PageVagas

--- src/Pages/Vagas.js
+++ src/Pages/Vagas.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, Form } from 'react-bootstrap';
+import { Jumbotron, Container, Form, Col, Row } from 'react-bootstrap';
 import './Pages.css';
 import Dados from '../Components/Vagas/Dados';
 import { useState, useEffect } from 'react';
@@ -14,29 +14,33 @@ export default function Vagas(props) {
         }
         fetchData();
     }, []);
-    
+
     return (
         <>
-            <Jumbotron className="text-center text-light jobs my-auto" fluid>
+            <Jumbotron className="text-center text-light jobs my-auto w-100 " fluid>
                 <Container fluid>
                     <h1 className="display-1 py-5 font">Nova Trilha</h1>
                     <h4 className="my-5">Procure uma oportunidade de emprego</h4>
                     <Form className="mx-auto w-50">
                         <Form.Group controlId="exampleForm.ControlInput1">
                             <Form.Control type="search" placeholder="Ex. Segurança, Porteiro." />
-                            <a href="/vagas" className="btn btn-danger mx-auto my-4">Pesquisar</a>
+                            <a href="/vagas" className="btn btn-outline-light  mx-auto my-4">Pesquisar</a>
                         </Form.Group>
                     </Form>
                 </Container>
             </Jumbotron>
-            
-            <div className="text-dark text-center py-4 bg-light">
-                <h1>Vagas Publicadas</h1>
-            </div>
 
-            {apiVagas && apiVagas.map(item => <Dados key={item.id} imagem={item.imagem} vaga={item.vaga} descricao={item.descricao} empregador={item.empregador} salario={item.salario} localizacao={item.localizacao}
-            />)}
 
+            <Container className="text-center bg-light" fluid>
+
+                <div className="py-4">
+                    <h1>Vagas Publicadas</h1>
+                </div>
+                <Row>
+                    {apiVagas && apiVagas.map(item => <Dados key={item.id} imagem={item.imagem} vaga={item.vaga} descricao={item.descricao} empregador={item.empregador} salario={item.salario} localizacao={item.localizacao}
+                    />)}
+                </Row>
+            </Container>
         </>
     );
 }
