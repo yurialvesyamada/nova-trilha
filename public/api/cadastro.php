e0baed50140c7fff2c5000bec5000dec65a63f76
Josiel Souza <josielsouza.dj@gmail.com>
2021-02-16 19:00:50 -0300

    alterar cadastro

--- public/api/cadastro.php
+++ public/api/cadastro.php
@@ -12,7 +12,9 @@ if (isset($_POST['cadastrar'])) {
     $email = filter_input(INPUT_POST, 'email');
     $senha = filter_input(INPUT_POST, 'senha');
     $telefone = filter_input(INPUT_POST, 'telefone');
+    $cidade = filter_input(INPUT_POST, 'cidade'); 
     $endereco = filter_input(INPUT_POST, 'endereco'); 
+    $escolaridade = filter_input(INPUT_POST, 'escolaridade'); 
     
 
     $sql = "SELECT * FROM usuario  WHERE `email` = '{$email}'  limit 1"; 
@@ -25,8 +27,14 @@ if (isset($_POST['cadastrar'])) {
         header("Refresh: 0;url=http://localhost:3000/cadastro?erro=existe");
       
     } else{
-      
-        $sql = "insert into usuario (nome,  email, senha, telefone, endereco) values ('$nome', '$email', '$senha','$telefone', '$endereco')";
+        
+
+        $sql = "INSERT INTO `nova-trilha`.`usuario` (`id_user`, `nome`, `email`, `senha`, `escolaridade`,  `funcao`,`id_telefone`,`id_endereco`,`id_cidade`)
+ SELECT `tb_telefone`.`telefone`, `tb_endereco`.`endereco`, `tb_cidade`.`cidade` FROM `tb_telefone`,`tb_endereco`,`tb_cidade`
+ where  `tb_telefone`.`id_telefone` & `tb_endereco`.`id_endereco` & `tb_cidade`.`id_cidade` = `nova-trilha`.`usuario`.`id_user` " ; 
+
+
+       // $sql = "insert into usuario (nome,  email, senha, telefone, endereco) values ('$nome', '$email', '$senha','$telefone', '$endereco')";
         $insert = $conn->query($sql);
         $conn->close();
         header("Refresh: 0;url=http://localhost:3000/vagas?erro=cadastro&?nome=$nome");
--- src/Pages/Cadastro.js
+++ src/Pages/Cadastro.js
@@ -14,7 +14,7 @@ export default function Cadastro() {
             <Erro />
             <Container className="text-center bg-light text-white links" fluid>
                 <Row>
-                    <Col md={6} lg={6} className="mx-auto my-5 rounded" style={{ background: `linear-gradient( #dc3545 20%, rgba(5, 7, 9, 0)150%), url(${somePic})` }}>
+                    <Col md={1} lg={11} className="mx-auto my-5 rounded" style={{ background: `linear-gradient( #dc3545 20%, rgba(5, 7, 9, 0)150%), url(${somePic})` }}>
                         <Col className="mx-auto my-5" sm={12} md={6} lg={6}>
                             <a href="/">
                                 <img src="/assets/img/nova-trilha-logo2.png" width="150px" alt="&copy; Nova Trilha" title="&copy; Nova Trilha" className="shadow-sm bg-white rounded" />
@@ -24,47 +24,84 @@ export default function Cadastro() {
                         <Form className="bg-danger px-5 m-4 rounded shadow-sm" method="POST" action={caminho + "/api/cadastro.php"}>
 
                             <h1 className="py-4">Cadastro</h1>
-                            <InputGroup className="pb-4">
+
+                            <InputGroup className="pb-4 py-3" as={Row} >
+                               <Form.Label column sm={1}>
+                                Nome 
+                                </Form.Label>
                                 <InputGroup.Prepend>
                                     <InputGroup.Text><i className="fas fa-user" title="Username"></i></InputGroup.Text>
                                 </InputGroup.Prepend>
                                 <FormControl name="nome" type="name" id="inlineFormInputGroup" placeholder="Nome" required />
                             </InputGroup>
 
-                            <InputGroup className="pb-4">
+                            <InputGroup className="pb-4 py-3"  as={Row} >
+                                <Form.Label column sm={1} >
+                                Email
+                                </Form.Label>
                                 <InputGroup.Prepend>
                                     <InputGroup.Text><i className="fas fa-envelope" title="E-mail"></i></InputGroup.Text>
                                 </InputGroup.Prepend>
                                 <FormControl name="email" type="email" id="inlineFormInputGroup" placeholder="E-mail" required />
-                            </InputGroup>
+                               
+                            </InputGroup>                           
+                                                                                   
+                            <Form.Row className="py-3 px-4">
 
-                            <InputGroup className="pb-4">
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text><i className="fas fa-key" title="Password"></i></InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl name="senha" type="password" placeholder="Senha" required />
-                            </InputGroup>
+                                <Form.Group className="px-5 "  as={Col} controlId="formGridEmail">
+                                <Form.Label>Senha</Form.Label>
+                                <Form.Control name="senha" type="password" placeholder="Senha" />
+                                </Form.Group>
 
-                            <InputGroup className="pb-4">
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text><i className="fas fa-mobile-alt"></i></InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl name="telefone" type="number" minLength="1" maxLength="12" id="inlineFormInputGroup" placeholder="Telefone" required />
-                            </InputGroup>
+                                <Form.Group className="px-2"  as={Col} controlId="formGridPassword">
+                                <Form.Label>telefone</Form.Label>
+                                <Form.Control  name="telefone"  type="number" placeholder="Telefone" />
+                                </Form.Group>
+                            </Form.Row>
+                                                                                          
+                            <Form.Row  className="py-3 px-4" > 
 
-                            <InputGroup className="pb-4">
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text><i className="fas fa-map-marker-alt"></i></InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl name="endereco" type="adress" id="inlineFormInputGroup" placeholder="Endereço" required />
-                            </InputGroup>
+                                <Form.Group className="px-5 " as={Col} controlId="formGridCity">
+                                <Form.Label>Cidade</Form.Label>
+                                <Form.Control name="cidade"  placeholder="Cidade"/>
+                                
+                                </Form.Group>
+                                <Form.Group className="px-2" type="adress" as={Col} controlId="formGridCity"   >
+                                <Form.Label>Endereço</Form.Label>
+                                <Form.Control name="endereco"  placeholder="Endereço"/>
+                                </Form.Group>
 
+                                
+                            </Form.Row >
+
+                            <Form.Group name="escolaridade" className="py-1 px-4 mx-5 pr-5" as={Col} controlId="formGridState">
+                                <Form.Label>Escolaridade</Form.Label>
+                                <Form.Control as="select" defaultValue="ensino">
+                                    <option>Ensino Fundamental Incompleto</option>
+                                    <option>Ensino Fundamental Completo</option>
+                                    <option>Ensino Médio Incompleto</option>
+                                    <option>Ensino Médio Completo</option>
+                                    <option>Ensino Técnico Incompleto</option>
+                                    <option>Ensino Técnico Completo</option>
+                                    <option>Ensino Superior Incompleto</option>
+                                    <option>Ensino Superior Completo</option>
+                                  
+                                </Form.Control>
+                                </Form.Group>                             
+
+                            <InputGroup className="py-3" as={Row} controlId="formHorizonta">
+                            <Form.Label column sm={3}>
+                            Função Pretendida 
+                            </Form.Label>
+                            <FormControl name="funcao" type="text"  placeholder="Função Pretendida " required />
+                            </InputGroup>
+                            
                             <div className="p-4">
                                 <Col>
                                     <Button href="login" className="px-5 mb-3 rounded-pill" type="submit" name="cadastrar" variant="outline-light">Cadastrar</Button>{' '}
                                 </Col>
                                 <h6>Já tem Cadastro? <a href="login">Entre aqui.</a></h6>
-                                <h6>Atualizar Cadastro -  <a href="Atualizar_cadastro">Clique aqui.</a></h6>
+                               
                             </div>
                         </Form>
                     </Col>
