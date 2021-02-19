d960a460ebeed89d84b570b65bba1c8ffe34a9c4
Yuri <you@teste.com>
2021-02-19 18:55:47 -0300

    Update Connection Bd

--- public/api/conexao.php
+++ public/api/conexao.php
@@ -5,15 +5,15 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Content-type: application/json");
 
 
-/* $servername = "localhost";
+$servername = "localhost";
 $username = "admin";
 $password = "1234";
-$dbname = "nova-trilha"; */
+$dbname = "nova-trilha";
  
-$servername = "sql202.epizy.com";
+/* $servername = "sql202.epizy.com";
 $username = "epiz_27734971";
 $password = "I7TIi8t187BLk";
-$dbname = "epiz_27734971_novatrilha";
+$dbname = "epiz_27734971_novatrilha"; */
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 $conn->query("set names utf8");
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$build = "http://novatrilha.infinityfreeapp.com";
-
- /* 
- $build = "http://localhost:3000";  */
+/* $build = "http://novatrilha.infinityfreeapp.com";
+ */
+ 
+ $build = "http://localhost:3000"; 
--- src/redux/caminho/caminhoReducer.js
+++ src/redux/caminho/caminhoReducer.js
@@ -1,8 +1,9 @@
 const initialState = {
     // Usar string vazia ao fazer o Build
     // Usar http://localhost ao usar o npm start
-    caminho: ""
-    /* caminho: "http://localhost/nova-trilha/public" */
+    /* caminho: "" */
+    
+    caminho: "http://localhost/nova-trilha/public"
 
 }
 
