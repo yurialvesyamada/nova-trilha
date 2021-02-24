998a48e043ab9214ed60d328c1777bdcbf065c34
Yuri <you@teste.com>
2021-02-24 17:17:29 -0300

    Update Page Recuperar Login

--- src/Pages/Recuperar_login.js
+++ src/Pages/Recuperar_login.js
@@ -1,9 +1,9 @@
 import Erro from '../Components/Erro/Erro';
-import { React, somePic } from 'react';
+import { React } from 'react';
 import { Form, Col, InputGroup, FormControl, Button, Container, Row, Image } from "react-bootstrap";
 import './Pages.css';
 import { useSelector } from 'react-redux';
-import novatrilha from '../assets/img/nova-trilha-logo2.png';
+import novatrilha from '../assets/img/nova-trilha-logo.png';
 
 
 export default function Recuperar_login() {
@@ -13,32 +13,34 @@ export default function Recuperar_login() {
         <>
             <Erro />
 
-            <Container className="text-center bg-light text-white links" fluid>
-                <Row>
-                    <Col md={6} lg={6} className="mx-auto my-5 rounded" style={{ background: `linear-gradient( #dc3545 20%, rgba(5, 7, 9, 0)150%), url(${somePic})` }}>
+            <Container className="text-center bg-light" fluid>
+                <Row className="py-5">
+                    <Col md={6} lg={6} className="mx-auto rounded">
 
                         <Col className="mx-auto my-5" sm={12} md={6} lg={6}>
                             <a href="/">
-                                <Image src={novatrilha} alt="&copy; Nova Trilha" title="&copy; Nova Trilha" className="shadow-sm rounded w-50" />
+                                <Image src={novatrilha} alt="&copy; Nova Trilha" title="&copy; Nova Trilha" className="shadow-sm  w-50 bg-info rounded" />
                             </a>
                         </Col>
 
-                        <Form className="bg-danger px-5 m-4 rounded shadow-sm" method="POST" action={caminho + "/api/Recuperar_login.php"}>
-
-                            <h1 className="py-4">Recuperar Senha</h1>
+                        <Form className="bg-white px-md-5 rounded shadow-sm border" method="POST" action={caminho + "/api/Recuperar_login.php"}>
+                            <div className="py-4 text-dark fonts h1">
+                                <i className="fas fa-lock"></i>
+                                <h1>Recuperar Senha</h1>
+                            </div>
                             <InputGroup className="pb-4">
                                 <InputGroup.Prepend>
-                                    <InputGroup.Text><i className="fas fa-envelope" title="E-mail"></i></InputGroup.Text>
+                                    <InputGroup.Text><i className="fas fa-envelope icon" title="E-mail"></i></InputGroup.Text>
                                 </InputGroup.Prepend>
                                 <FormControl name="email" type="email" id="inlineFormInputGroup" placeholder="E-mail" required />
                             </InputGroup>
 
-                            <div className="p-4">
-                                <Col>
-                                    <Button className="px-5 mb-3 rounded-pill" type="submit" name="confirmarsenha" value="confirmar" variant="light">Confirmar</Button>{' '}
+                            <div>
+                                <Col md={5} className="py-3 mx-auto">
+                                    <Button className="rounded w-100" type="submit" name="confirmarsenha" value="confirmar" variant="info">Recuperar</Button>{' '}
                                 </Col>
-                                <Col>
-                                    <Button href="/login" className="px-4 mb-3 rounded-pill" type="submit" name="cancelar" variant="outline-light">Cancelar</Button>{' '}
+                                <Col md={4} className="py-3 mx-auto">
+                                    <Button href="/login" className="rounded w-100" type="submit" name="cancelar" variant="outline-info">Voltar</Button>{' '}
                                 </Col>
                             </div>
                         </Form>
