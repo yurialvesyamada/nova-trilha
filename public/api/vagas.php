7ed231c406610124b9f8f0475d99dc2b54b95e50
Yuri Alves Yamada <yurialvesyamada@hotmail.com>
2022-02-22 23:23:45 -0300

    fix: Add files to gitignore list

--- public/api/vagas.php
+++
@@ -1,6 +0,0 @@
-<?php require('./conexao.php');
-
-$sql = "SELECT * FROM vagas";
-$result = $conn->query($sql);
-echo json_encode($result->fetch_all(MYSQLI_ASSOC));
-   
--- public/api/vagasempresas.php
+++
@@ -1,5 +0,0 @@
-<?php require('./conexao.php');
-
-$sql = "SELECT * FROM vagas where empregador = 'Animalias Pet Center'";
-$result = $conn->query($sql);
-echo json_encode($result->fetch_all(MYSQLI_ASSOC));
